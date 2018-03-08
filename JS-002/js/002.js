a93df3446ae2c81f571fe776894da7409e69bb17
yangshijia1 <1064414214@qq.com>
2018-03-08 18:09:09 +0800

    课下练习002

---
+++ JS-002/js/002.js
@@ -0,0 +1,7 @@
+console.log(typeof 123);
+console.log(typeof ture);
+console.log(typeof "abc");
+console.log(typeof null);
+console.log(undefined);
+console.log(typeof {name:"Mike",age:20});
+console.log(typeof function foo(){});
