3c50621db937982dcde198c9ec44f9730caf27b0
yangshijia1 <1064414214@qq.com>
2018-03-15 08:00:16 +0800

    练习

--- JS-002/js/002.js
+++ JS-002/js/002.js
@@ -1,7 +1,20 @@
-console.log(typeof 123);
-console.log(typeof ture);
-console.log(typeof "abc");
-console.log(typeof null);
-console.log(undefined);
-console.log(typeof {name:"Mike",age:20});
-console.log(typeof function foo(){});
+// console.log(typeof 123);
+// console.log(typeof ture);
+// console.log(typeof "abc");
+// console.log(typeof null);
+// console.log(undefined);
+// console.log(typeof {name:"Mike",age:20});
+// console.log(typeof function foo(){});
+
+
+var arr = [1,2,3,4,5];
+arr.length = 1;
+console.log(arr,arr.length);
+
+
+var str = "abcde";
+console.log(str.length);
+str.length = 1;
+console.log(str.length,str);
+
+
